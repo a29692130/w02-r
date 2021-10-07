ae6b3c90f089c59d7a2f063d9d8696e2879088ed
a29692130 <a29692130@gmail.com>
2021-10-07 13:33:03 +0800

    first commit

---
+++ src/App_41.js
@@ -0,0 +1,15 @@
+import Todo_41 from './components/Todo_41';
+import './App.css';
+
+function App_41() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo_41 text="Learn React" />
+      <Todo_41 text="Learn SQL" />
+      <Todo_41 text="Learn NextJS" />
+    </div>
+  );
+}
+
+export default App_41;
